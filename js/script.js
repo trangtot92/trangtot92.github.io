48f60da654e84202889234071fb0de84e71e4a63
Trangtot92 <tran.thi.trang@framgia.com>
2019-03-21 15:00:17 +0700

    Init

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+
+
+	/* ---- Countdown timer ---- */
+
+	$('#counter').countdown({
+		timestamp : 1556557200000
+	});
+
+
+	/* ---- Animations ---- */
+
+	$('#links a').hover(
+		function(){ $(this).animate({ left: 3 }, 'fast'); },
+		function(){ $(this).animate({ left: 0 }, 'fast'); }
+	);
+
+	$('footer a').hover(
+		function(){ $(this).animate({ top: 3 }, 'fast'); },
+		function(){ $(this).animate({ top: 0 }, 'fast'); }
+	);
+
+
+
+
+});
